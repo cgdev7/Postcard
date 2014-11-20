b2eb6cf9626a2e55aff6b21f9ef75101514c5bcb
Cliff Gaw <cliff.gaw@gmail.com>
2014-11-19 22:45:36 -0800

    added comment to test commits

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -27,6 +27,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //added comment to check commit
+        
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
